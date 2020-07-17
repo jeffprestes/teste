41cddacd0007a34324a0d5c222951f88741a5d9a
Jeff Prestes <jeffprestes@gmail.com>
2020-07-16 21:05:02 -0300

    Add files via upload

---
+++ blockchain.js
@@ -0,0 +1,14 @@
+function voltaFundo() {
+  $("#formImovel").css("background-color", "yellow");
+  $("h1").html("Registro Eletronico de Imoveis");
+}
+
+function salvarRegistro() {
+  event.preventDefault();
+  alert("Em construção...");
+}
+
+function easterEgg() {
+  $("#formImovel").css("background-color", "pink");
+  $("h1").html("Nathaly rulez...");
+}
