c06d1267dfad03879f613f7df94ade216d7a4d2b
Jeff Prestes <jeffprestes@gmail.com>
2020-07-21 15:23:01 -0300

    Atualiza conexao

--- blockchain-infra.js
+++ blockchain-infra.js
@@ -1,3 +1,13 @@
+//Definicoes de variaveis para toda a pagina (ou site)
+//Variaveis essas relacionadas a operações com Metamask e Contratos Inteligentes no Ethereum
+var contaAtual;
+var provedorDeSignatarios;
+var signatario;
+var enderecoContratoInteligente;
+
+/*
+FUNCOES RELACIONADAS A OPERACOES COM METAMASK E CONTRATOS INTELIGENTES NO ETHEREUM
+*/
 function conectaAoMetamask() {
   event.preventDefault();
   console.log("conectaAoMetamask chamado");
@@ -12,7 +22,7 @@ function conectaAoMetamask() {
 function requisitaAcessoAContas() {
   ethereum
     .send("eth_requestAccounts")
-    .then(gerenciaTrocaDeSelecaoDeEndereco)
+    .then(gerenciaTrocaDeSelecaoDeContas)
     .catch((err) => {
       if (err.code === 4001) {
         // EIP 1193 userRejectedRequest error
@@ -21,13 +31,13 @@ function requisitaAcessoAContas() {
         console.error(err);
       }
     });
-  ethereum.on("accountsChanged", gerenciaTrocaDeSelecaoDeEndereco);
+  ethereum.on("accountsChanged", gerenciaTrocaDeSelecaoDeContas);
 }
 
-function gerenciaTrocaDeSelecaoDeEndereco(_contas) {
+function gerenciaTrocaDeSelecaoDeContas(_contas) {
   console.log("gerenciaTrocaDeSelecaoDeEndereco", _contas.length);
   provedorDeSignatarios = new ethers.providers.Web3Provider(web3.currentProvider);
-  signatario = providerSigner.getSigner();
+  signatario = provedorDeSignatarios.getSigner();
   //contractWithSigner = new ethers.Contract(contractAddress, contractAbi, signer);
   if (_contas.length === 0) {
     alert("Por favor instale o MetaMask em metamask.io");
@@ -35,6 +45,9 @@ function gerenciaTrocaDeSelecaoDeEndereco(_contas) {
     contaAtual = _contas[0];
     if (contaAtual) {
       console.log("gerenciaTrocaDeSelecaoDeEndereco objects", _contas, contaAtual, signer);
+      $("#btnSalvar").en;
     }
   }
 }
+
+var abiContratoInteligente;
